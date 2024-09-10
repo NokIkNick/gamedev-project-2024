396e5f9a5018f921f5053b346e7e4c29bdd9ef9a
chris4567890 <starchristian4@hotmail.com>
2024-09-10 22:56:34 +0200

    made enemy jump
    made it possible for the enemy to jump by checking the y position of itself compared to the player position. slight buck: when colliding with the player enemy moves backward for some reason. instead of keep trying to move forward which is hillarious to watch.

--- gamedevproject2024/Assets/Scripts/AiChase.cs
+++ gamedevproject2024/Assets/Scripts/AiChase.cs
@@ -1,16 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AiChase : MonoBehaviour
 {
     public GameObject player;
     public float speed;
-
+    public float jumpForce;
+    public LayerMask obstacleLayer;
     private float distance;
+    private Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,5 +26,20 @@ public class AiChase : MonoBehaviour
 
         transform.position = Vector2.MoveTowards(this.transform.position,player.transform.position,speed*Time.deltaTime);
         transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+
+        if (player.transform.position.y > transform.position.y)
+        {
+            Jump();
+        }
+
     }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    
+
+
 }
