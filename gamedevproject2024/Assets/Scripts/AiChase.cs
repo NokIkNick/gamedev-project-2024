0a717b259970716e14d4926610b332851004c6b5
chris4567890 <starchristian4@hotmail.com>
2024-09-12 11:28:27 +0200

    removed some stuff
    have done some cleanup thingymajinks

--- gamedevproject2024/Assets/Scripts/AiChase.cs
+++ gamedevproject2024/Assets/Scripts/AiChase.cs
@@ -1,5 +1,9 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 public class AiChase : MonoBehaviour
 {
@@ -7,29 +11,40 @@ public class AiChase : MonoBehaviour
     public float speed;
     public float jumpForce;
     public LayerMask obstacleLayer;
-    private float distance;
+   // private float distance;
     private Rigidbody2D rb;
+    
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");  
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
-        direction.Normalize();
-        float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
+     //   distance = Vector2.Distance(transform.position, player.transform.position);
+        //Vector2 direction = player.transform.position - transform.position;
+        //direction.Normalize();
+        //float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
 
-        transform.position = Vector2.MoveTowards(this.transform.position,player.transform.position,speed*Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
-
+        //transform.position = Vector2.MoveTowards(this.transform.position,player.transform.position,speed*Time.deltaTime);
+        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+        Vector2 currentDirection = Vector2.Lerp(transform.position, direction, Time.deltaTime * 40f);
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        RaycastHit2D hitter = Physics2D.Raycast(transform.position, Vector2.right);
+        if (hitter.collider != null) 
+        {
+            
+        }
         if (player.transform.position.y > transform.position.y)
         {
-            Jump();
+            //Jump();
         }
 
     }
